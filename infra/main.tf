0207157b06693a336acbff738883f7644b1a6782
Gayathri-R-1527 <gayathri.r.2017.cse@ritchennai.edu.in>
2026-07-20 08:15:36 -0500

    added vnet

--- infra/main.tf
+++ infra/main.tf
@@ -9,6 +9,7 @@ resource "azurerm_storage_account" "sa" {
   location                 = azurerm_resource_group.main.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+  
 }
 
 resource "azurerm_storage_container" "uploads" {
@@ -36,6 +37,7 @@ resource "azurerm_mssql_server" "sql" {
   version                      = "12.0"
   administrator_login          = var.sql_admin_username
   administrator_login_password = var.sql_admin_password
+  public_network_access_enabled = false
 }
 
 resource "azurerm_mssql_database" "sqldb" {
@@ -95,6 +97,7 @@ resource "azurerm_linux_web_app" "api" {
       node_version = "20-lts"
     }
     app_command_line = "node server.js"
+    vnet_route_all_enabled = true
   }
 
   app_settings = {
@@ -132,6 +135,7 @@ resource "azurerm_linux_function_app" "func" {
     application_stack {
       python_version = "3.11"
     }
+   
   }
 
   app_settings = {
@@ -155,6 +159,7 @@ resource "azurerm_key_vault" "kv" {
   sku_name                   = "standard"
   tenant_id                  = data.azurerm_client_config.current.tenant_id
   enable_rbac_authorization  = true
+  public_network_access_enabled = false
 }
 
 resource "azurerm_role_assignment" "api_kv_secrets_user" {
@@ -179,4 +184,131 @@ resource "azurerm_role_assignment" "user_kv_admin" {
   scope                = azurerm_key_vault.kv.id
   role_definition_name = "Key Vault Administrator"
   principal_id         = "5167feb5-68ca-4bfc-a9da-ceaccb3e5de6"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-${var.project_name}-${var.environment}"
+  address_space       = ["10.10.0.0/16"]
+  location            = "Central US"
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+resource "azurerm_subnet" "appservice_subnet" {
+  name                 = "snet-appservice-${var.environment}"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.10.1.0/24"]
+
+  delegation {
+    name = "appservice-delegation"
+    service_delegation {
+      name    = "Microsoft.Web/serverFarms"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
+    }
+  }
+}
+
+resource "azurerm_subnet" "privateendpoint_subnet" {
+  name                 = "snet-privateendpoint-${var.environment}"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.10.2.0/24"]
+}
+
+resource "azurerm_private_dns_zone" "kv_dns" {
+  name                = "privatelink.vaultcore.azure.net"
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "kv_dns_link" {
+  name                  = "kv-dns-link-${var.environment}"
+  resource_group_name   = azurerm_resource_group.main.name
+  private_dns_zone_name = azurerm_private_dns_zone.kv_dns.name
+  virtual_network_id    = azurerm_virtual_network.vnet.id
+}
+
+resource "azurerm_private_endpoint" "kv_pe" {
+  name                = "pe-kv-${var.project_name}-${var.environment}"
+  location            = "Central US"
+  resource_group_name = azurerm_resource_group.main.name
+  subnet_id           = azurerm_subnet.privateendpoint_subnet.id
+
+  private_service_connection {
+    name                           = "kv-privateserviceconnection"
+    private_connection_resource_id = azurerm_key_vault.kv.id
+    subresource_names              = ["vault"]
+    is_manual_connection           = false
+  }
+
+  private_dns_zone_group {
+    name                 = "kv-dns-zone-group"
+    private_dns_zone_ids = [azurerm_private_dns_zone.kv_dns.id]
+  }
+}
+resource "azurerm_app_service_virtual_network_swift_connection" "api_vnet_integration" {
+  app_service_id = azurerm_linux_web_app.api.id
+  subnet_id      = azurerm_subnet.appservice_subnet.id
+}
+
+
+resource "azurerm_private_dns_zone" "sql_dns" {
+  name                = "privatelink.database.windows.net"
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "sql_dns_link" {
+  name                  = "sql-dns-link-${var.environment}"
+  resource_group_name   = azurerm_resource_group.main.name
+  private_dns_zone_name = azurerm_private_dns_zone.sql_dns.name
+  virtual_network_id    = azurerm_virtual_network.vnet.id
+}
+
+resource "azurerm_private_endpoint" "sql_pe" {
+  name                = "pe-sql-${var.project_name}-${var.environment}"
+  location            = "Central US"
+  resource_group_name = azurerm_resource_group.main.name
+  subnet_id           = azurerm_subnet.privateendpoint_subnet.id
+
+  private_service_connection {
+    name                           = "sql-privateserviceconnection"
+    private_connection_resource_id = azurerm_mssql_server.sql.id
+    subresource_names              = ["sqlServer"]
+    is_manual_connection           = false
+  }
+
+  private_dns_zone_group {
+    name                 = "sql-dns-zone-group"
+    private_dns_zone_ids = [azurerm_private_dns_zone.sql_dns.id]
+  }
+}
+
+resource "azurerm_private_dns_zone" "storage_dns" {
+  name                = "privatelink.blob.core.windows.net"
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "storage_dns_link" {
+  name                  = "storage-dns-link-${var.environment}"
+  resource_group_name   = azurerm_resource_group.main.name
+  private_dns_zone_name = azurerm_private_dns_zone.storage_dns.name
+  virtual_network_id    = azurerm_virtual_network.vnet.id
+}
+
+resource "azurerm_private_endpoint" "storage_pe" {
+  name                = "pe-storage-${var.project_name}-${var.environment}"
+  location            = "Central US"
+  resource_group_name = azurerm_resource_group.main.name
+  subnet_id           = azurerm_subnet.privateendpoint_subnet.id
+
+  private_service_connection {
+    name                           = "storage-privateserviceconnection"
+    private_connection_resource_id = azurerm_storage_account.sa.id
+    subresource_names              = ["blob"]
+    is_manual_connection           = false
+  }
+
+  private_dns_zone_group {
+    name                 = "storage-dns-zone-group"
+    private_dns_zone_ids = [azurerm_private_dns_zone.storage_dns.id]
+  }
 }
